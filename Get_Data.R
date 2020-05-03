eb70b6e13cce44da1913fc8d82146bda93f0e5b9
tobiasdahlnielsen <43407100+tobiasdahlnielsen@users.noreply.github.com>
2020-05-03 10:42:52 +0200

    simple returns

--- Get_Data.R
+++ Get_Data.R
@@ -1,4 +1,5 @@
-library(ghyp)
+library(ghyp);library(tictoc);library(tidyverse);library(magrittr)
+
 setwd("~/P8")
 
 getdbdata = function(series,path = ""){
@@ -16,7 +17,7 @@ logreturns = function(df){
 }
 simreturns =  function(df){
   library(magrittr)
-  df %<>% mutate(simpler = logr %>% exp + 1 )
+  df %<>% mutate(simr = logr %>% exp - 1 )
   return(df)
 }
 
@@ -27,35 +28,94 @@ HBAN = getdbdata("HBAN")
 SPY  = getdbdata("SPY")
 
 
-library(tidyverse);library(magrittr)
 
 
 ACAS %<>% logreturns %<>% simreturns
-HBAN %<>% logreturns# %<>% simreturns
-SPY  %<>% logreturns# %<>% simreturns
+HBAN %<>% logreturns %<>% simreturns
+SPY  %<>% logreturns %<>% simreturns
 
 
-allreturns <- cbind(SPY$returns,ACAS$returns,HBAN$returns)
+alllogreturns <- cbind(SPY$logr,ACAS$logr,HBAN$logr)
+allsimreturns <- cbind(SPY$simr,ACAS$simr,HBAN$simr)
 
-tic()
-fittetdist <- fit.NIGmv(allreturns[1:391,],silent=TRUE)
-toc()
-
-dghyp(rep(0.95,3), object = fittetdist)
-
-ESghyp(0.05, object = fittetdist, distr = c("return", "loss"))
-
-optvalues <- portfolio.optimize(fittetdist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return")
-
-ACAS %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price)
-HBAN %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price)
-SPY  %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price)
+ACAS %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
+HBAN %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
+SPY  %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
 
 O = which(SPY[["Time"]] == SPY[["Time"]][1] + days(1)) - 1
 
-ACAS %>% ggplot(aes(x = Time, y = Price)) + geom_line()
-HBAN %>% ggplot(aes(x = Time, y = Price)) + geom_line()
-SPY  %>% ggplot(aes(x = Time, y = Price)) + geom_line()
+#378
+tic()
+for (i in 1:378) {
+  print(i)
+  if (i==1) {
+    dist <- fit.NIGmv(alllogreturns[1:O*2,],silent=TRUE)
+    optvalues <- portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE)
+    portreturns <- allsimreturns[1:O*2,] %*% optvalues$opt.weights
+  }
+  # else if (i==27|i==28|i==32|i==38|i==42|i==83|i==83
+  #          |i==126|i==141|i==142|i==145|i==146|i==148|i==149|i==150|i==151) {
+  #   print("error")
+  # }
+  else {
+    dist <- fit.NIGmv(alllogreturns[(O*2*(i-1)+1):(O*2*i),],silent=TRUE)
+    optvalues <-try(portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE),silent=TRUE)
+    if (class(optvalues)!="try-error") {
+      j=i
+    }
+    if (class(optvalues)=="try-error") {
+      dist <- fit.NIGmv(alllogreturns[(O*2*(j-1)+1):(O*2*j),],silent=TRUE)
+      optvalues <-portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE)
+    }
+    portreturns <- c(portreturns, allsimreturns[(O*2*(i-1)+1):(O*2*i),] %*% optvalues$opt.weights)
+    }
+}
+toc()
+
+
+#756
+tic()
+for (i in 651:756) {
+  print(i)
+  if (i==1) {
+    dist <- fit.NIGmv(alllogreturns[1:391,],silent=TRUE)
+    optvalues <- portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE)
+    portreturns <- allsimreturns[1:391,] %*% optvalues$opt.weights
+  }
+  # else if (i==27|i==28|i==32|i==38|i==42|i==83|i==83
+  #          |i==126|i==141|i==142|i==145|i==146|i==148|i==149|i==150|i==151) {
+  #   print("error")
+  # }
+  else {
+    dist <- fit.NIGmv(alllogreturns[(391*(i-1)+1):(391*i),],silent=TRUE)
+    optvalues <-try(portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE),silent=TRUE)
+    if (class(optvalues)!="try-error") {
+      j=i
+    }
+    if (class(optvalues)=="try-error") {
+      dist <- fit.NIGmv(alllogreturns[(391*(j-1)+1):(391*j),],silent=TRUE)
+      optvalues <-portfolio.optimize(dist,risk.measure = "expected.shortfall",type = "minimum.risk",distr = "return",silent = TRUE)
+    }
+    portreturns <- c(portreturns, allsimreturns[(391*(i-1)+1):(391*i),] %*% optvalues$opt.weights)
+  }
+}
+toc()
+
+
+
+
+
+
+ACAS %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
+HBAN %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
+SPY  %<>% mutate(Time = ymd_hms(utcsec), Price = price) %>% select(Time,Price,logr,simr)
+
+O = which(SPY[["Time"]] == SPY[["Time"]][1] + days(1)) - 1
+
+ACAS %>% ggplot(aes(x = Time, y = Price)) + geom_line()+ggtitle("ACAS")
+HBAN %>% ggplot(aes(x = Time, y = Price)) + geom_line()+ggtitle("HBAN")
+SPY  %>% ggplot(aes(x = Time, y = Price)) + geom_line()+ggtitle("SPY")
+
 
 
 
