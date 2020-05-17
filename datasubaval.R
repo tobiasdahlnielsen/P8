61ceedd9c640e9c3fab957ad2a475e6ab289f5f4
tobiasdahlnielsen <43407100+tobiasdahlnielsen@users.noreply.github.com>
2020-05-17 18:06:34 +0200

    validation

--- datasubaval.R
+++ datasubaval.R
@@ -4,9 +4,16 @@ load("~/P8/cleandata.RData")
 
 
 HBAN_fit = HBAN %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
-ACAS_fit = ACAS %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM",paramStart = HBAN_fit$param)
-SPY_fit = HBAN %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
+ACAS_fit = ACAS %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
+SPY_fit  = SPY  %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
 
+HBAN_fit = HBAN %>% pull(logr) %>% .[-1] %>% fit.NIGuv(silent=T)
+ACAS_fit = ACAS %>% pull(logr) %>% .[-1] %>% fit.NIGuv(silent=T)
+SPY_fit  = SPY  %>% pull(logr) %>% .[-1] %>% fit.NIGuv(silent=T)
+
+ghyp::qqghyp(HBAN_fit,gaussian =F)
+ghyp::qqghyp(ACAS_fit,gaussian =F)
+ghyp::qqghyp(SPY_fit,gaussian =F)
 
 x = seq(from = -0.01, to = 0.01, length.out = 1000)
 y_ACAS = sapply(x,function(x){dnig(x,param = ACAS_fit$param)})
@@ -26,12 +33,22 @@ lines(x,y_SPY, col = "red")
 #Subset Every 5th minute 
 ACAS_5minute = ACAS %>% filter(minute(Time) %% 5 == 0)
 HBAN_5minute = HBAN %>% filter(minute(Time) %% 5 == 0)
-SPY_5minute = SPY %>% filter(minute(Time) %% 5 == 0)
+SPY_5minute  = SPY  %>% filter(minute(Time) %% 5 == 0)
+O = which(SPY_5minute[["Time"]] == SPY_5minute[["Time"]][1] + days(1)) - 1
 
-
-ACAS_5minute_fit = ACAS_5minute %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
+ACAS_5minute_fit = ACAS_5minute[1:(O*500),] %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
 HBAN_5minute_fit = HBAN_5minute %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
-SPY_5minute_fit = HBAN_5minute %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
+SPY_5minute_fit = SPY_5minute %>% pull(logr) %>% .[-1] %>% nigFit(startValues = "MoM")
+
+ACAS_5minute_fit = ACAS_5minute %>% pull(logr) %>% 
+  .[-1] %>% fit.NIGuv(silent=T,alpha.bar=0.01,opt.pars = c(alpha.bar=F))
+HBAN_5minute_fit = HBAN_5minute[1:(O*500),] %>% pull(logr) %>% .[-1] %>% fit.NIGuv(silent=T)
+SPY_5minute_fit = SPY_5minute[1:(O*500),] %>% pull(logr) %>% .[-1] %>% fit.NIGuv(silent=T)
+
+ghyp::qqghyp(HBAN_5minute_fit,gaussian =F)
+ghyp::qqghyp(ACAS_5minute_fit,gaussian =F)
+ghyp::qqghyp(SPY_5minute_fit,gaussian =F)
+
 
 
 x = seq(from = -0.01, to = 0.01, length.out = 1000)
